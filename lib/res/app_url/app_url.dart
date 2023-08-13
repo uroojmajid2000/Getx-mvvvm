241a53ec651edb52b39aabf11d919b5c8252c7ee
uroojmajid2000 <urooj.majid2000@gmail.com>
2023-08-12 19:15:17 -0700

    first commit

---
+++ lib/res/app_url/app_url.dart
@@ -0,0 +1,6 @@
+class AppUrl {
+  static const String baseUrl = 'https://reqres.in';
+  static const String loginApi = '$baseUrl/api/login';
+
+  static const String userListApi = 'https://reqres.in/api/users?page=2';
+}
